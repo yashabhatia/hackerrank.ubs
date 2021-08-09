913d71a614078dc76eaea4900ee26cd25eeaa976
yashabhatia <39289952+yashabhatia@users.noreply.github.com>
2021-08-09 21:37:09 +0530

    test commit

--- FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
+++ FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
@@ -7,6 +7,7 @@ public class FurnitureOrder implements FurnitureOrderInterface {
      * TODO: Create a map of Furniture items to order quantities
      */
 
+    Map<String,Integer> orderMap = new HashMap<>();
     /**
      * Initialize a new mapping of Furniture types to order quantities.
      */
